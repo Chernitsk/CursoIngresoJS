67da1342580659e7e4110950b919fd97292d5ca8
Chernitsk <98746939+Chernitsk@users.noreply.github.com>
2022-01-31 18:02:54 -0300

    ejercicio 1-7

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,37 @@
 /*
+Tschernitschek Brian DIV E
+E/S Ej6
+
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+ //creo/nombro las variables
+ var num1;
+ var num2;
+ var resultado;
+
+
+//defino las variables por Id
+ num1=txtIdNumeroUno.value;
+ num2=txtIdNumeroDos.value;
+
+
+ //cambia la variable a numero
+ num1=parseInt(num1);
+ num2=parseInt(num2);
+
+
+//doy a entender que el resultado es las suma/resta de las variables creadas
+resultado= num1 + num2;
+
+
+//muestro el resultado de la cuenta realizada
+alert("El resultado es=" + resultado);
+
+
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,25 +1,86 @@
 /*
+Tschernitschek Brian DIV E
+E/S Ej7
+
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	//creo/nomro variables
+	var num1;
+	var num2;
+    var resultado;
+
+    //defino variables
+    num1=txtIdNumeroUno.value;
+    num2=txtIdNumeroDos.value;
+
+    //hago que los numeros no se concatenen
+    num1=parseInt(num1);
+    num2=parseInt(num2);
+
+    //indico que el resultado es igual a num1 + num2
+    resultado= num1 + num2;
+
+
+    //ventana emergente donde dice el resultado de la cuenta hecha
+    alert("El resultado es:" + resultado);
+
+
 }
 
 function restar()
 {
-	
+	var num1
+	var num2
+	var resultado
+
+	num1=txtIdNumeroUno.value;
+	num2=txtIdNumeroDos.value;
+
+	num1=parseInt(num1)
+    num2=parseInt(num2)	
+
+    resultado= num1 - num2;
+
+    alert("El resultado es:" + resultado);
 }
 
 function multiplicar()
 { 
-	
+	var num1
+	var num2
+	var resultado
+
+	num1=txtIdNumeroUno.value;	
+	num2=txtIdNumeroDos.value;
+
+	num1=parseInt(num1)
+    num2=parseInt(num2)	
+
+    resultado= num1 * num2;
+
+    alert("El resultado es:" + resultado);
 }
+
 
 function dividir()
 {
-	
+	var num1
+	var num2
+	var resultado
+     
+    num1=txtIdNumeroUno.value;
+    num2=txtIdNumeroDos.value;
+
+    num1=parseInt(num1)
+    num2=parseInt(num2)	
+
+    resultado= num1 / num2;
+
+    alert("El resultado es:" + resultado);
 }
+
 
